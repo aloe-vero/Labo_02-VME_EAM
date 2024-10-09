d478cc22083f638a0cef8cd8aead1aee1c91f2cd
aloe-vero <mendoza.everonica@gmail.com>
2024-10-09 14:37:08 -0400

    ProduitModele et Utilisateur modele

---
+++ php/Models/ProduitModel.php
@@ -0,0 +1,23 @@
+<?php
+class ProduitModel {
+private $db;
+
+public function __construct($database) {
+$this->db = $database;
+}
+public function getAllProduits() {
+
+}
+
+public function createProduit() {
+
+}
+
+public function updateProduit() {
+
+}
+
+public function deleteProduit() {
+
+}
+}
---
+++ php/Models/UtilisateurModel.php
@@ -0,0 +1,24 @@
+<?php
+
+class UtilisateurModel {
+    private $db;
+
+    public function __construct($database) {
+        $this->db = $database;
+    }
+    public function getAllUtilisateurs() {
+
+    }
+
+    public function createUtilisateur() {
+
+    }
+
+    public function updateUtilisateur() {
+
+    }
+
+    public function deleteUtilisateur() {
+
+    }
+}
