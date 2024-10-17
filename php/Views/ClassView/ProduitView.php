22f68c5712676e3da88349e34e5319aa7c2ea7ed
aloe-vero <mendoza.everonica@gmail.com>
2024-10-17 13:37:01 -0400

    UI

--- php/Views/ClassView/ProduitView.php
+++ php/Views/ClassView/ProduitView.php
@@ -8,14 +8,18 @@ class ProduitView
     {
 
         foreach ($produits as $produit) {
-            echo "<div class='carte'><img src={$produit['image']} ><p>{$produit['description']}</p></div>";
+            echo "<div class='produit'>
+                    <img src={$produit['image']} alt='polo licorne' width='250' height='280' >
+                    <p class='description realistic-marker-highlight'>{$produit['description']}</p>
+                    <p class='prix'>{$produit['prix']}</p>
+                </div>";
         }
 
     }
 
     public function displayProduit($produits){
         foreach ($produits as $produit) {
-        echo "<div class='carte'><img src={$produit['image']} ><p>{$produit['description']}</p><p>{$produit['taille']}</p><p>{$produit['type']}</p><p>{$produit['prix']}</p><p>{$produit['couleur']}</p></div>";
+        echo "<div class='produit'><img src={$produit['image']} ><p>{$produit['description']}</p><p>{$produit['taille']}</p><p>{$produit['type']}</p><p>{$produit['prix']}</p><p>{$produit['couleur']}</p></div>";
     }
 
     }
