7623998a20ed104ba2d9ddee766964f3cd9a5224
aloe-vero <mendoza.everonica@gmail.com>
2024-10-18 14:38:11 -0400

    Connexion et inscription

--- php/Views/inscription.php
+++ php/Views/inscription.php
@@ -5,23 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/styleInscription.css?version=1" />
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
-    <title>Inscription — Rich Ricasso</title>
+    <title>Rich Ricasso</title>
 </head>
 <body>
     <div class="container">
         <?php include 'partials/header.php'; ?>
         <div class="connexion">
             <p id="titre">CRÉER UN COMPTE</p>
-            <form action="" id="connexionForm" method="POST">
+            <form action="../registrationCompte.php" id="connexionForm" method="POST">
                 <input id="prenom" name="prenom" type="text" placeholder="PRENOM">
                 <br>
                 <input id="nomDeFamille" name="nomDeFamille" type="text" placeholder="NOM DE FAMILLE">
                 <br>
-                <input id="email" name="email" type="text" placeholder="ADRESSE EMAIL">
+                <input id="email" name="email" type="text" placeholder="COURRIEL">
                 <br>
-                <input id="mdp" name="mdp" type="text" placeholder="MOT DE PASSE">
+                <input id="mdp" name="mdp" type="password" placeholder="MOT DE PASSE">
                 <br>
-                <button type="button" id="buttonConnexion" onclick="window.location.href='connexion.php';">CONNEXION</button>
                 <input id="buttonInscription" type="submit" value="S'INSCRIRE">
             </form>
         </div>
--- php/registrationCompte.php
+++ php/registrationCompte.php
@@ -1,34 +1,34 @@
 <?php
 $conn = new PDO('mysql:host=localhost:3306;dbname=boutique_vetements', 'root');
 $prenom = $_POST['prenom'];
-$nom = $_POST['nom'];
+$nom = $_POST['nomDeFamille'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = $_POST['mdp'];
 
 
-require "php/Controllers/UtilisateurController.php";
+require "Controllers/UtilisateurController.php";
 
 $uc = new UtilisateurController($conn);
 
 
 if (empty($prenom) || empty($nom) || empty($email)|| empty($password)) {
     echo "Tous les champs sont obligatoires.";
-    exit(); // Met fin au script courant
+    exit();
 }
+
+
+
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "L'adresse e-mail n'est pas valide.";
     exit();
+}else if(count($uc->getUtilisateurByCourriel($email)) > 0){
+    echo"Address courriel existant";
 }
+
 else {
+
     $hash = password_hash($password, PASSWORD_DEFAULT);
     $uc ->createUtilisateur($nom, $prenom, $hash, $email);
-
-
+    header("Location: Views/connexion.php");
 }
-
-// Enregistrer les données dans une base de données MySQL
-
-echo "<h2>Merci, $prenom $nom.</h2>";
-echo "<p>Votre adresse email est : <b>$email.</b></p>";
-?>
